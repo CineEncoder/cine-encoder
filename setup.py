9479f1e2d340412a0b318ed2f8d84c6c082e406d
helg1980 <123>
2020-07-22 13:45:06 +0200

    Create icons

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: Python Software Foundation License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
