9624b57801d668d335bb71181d32942685bf466e
helg1980 <123>
2020-08-26 22:58:11 +0200

    Renew main.py ver. 2.2

--- cine-encoder/main.py
+++ cine-encoder/main.py
@@ -614,9 +614,9 @@ class ExampleApp(QtWidgets.QMainWindow, ui_main.Ui_MainWindow):
 
         if self.comboBox_1.currentText() == "H265  10 bit" and self.comboBox_3.currentText() == "CPU": #-------H265-10bit
             if self.comboBox_4.currentText() == "CQP":
-                preset = str(f'-b:v 0 -pix_fmt yuv420p10le -c:v libx265 -preset slow -rc vbr_hq -cq {quality} -qmin {quality} -qmax {quality} -rc-lookahead 32 -sei hdr -profile:v main10 {colorprim}{transfer}{colormatrix}{color_range}-max_muxing_queue_size 1024 {apreset} -f matroska ')
+                preset = str(f'-b:v 0 -pix_fmt yuv420p10le -c:v libx265 -preset slow -rc vbr_hq -cq {quality} -qmin {quality} -qmax {quality} -rc-lookahead 32 -profile:v main10 {colorprim}{transfer}{colormatrix}{color_range}-max_muxing_queue_size 1024 {apreset} -f matroska ')
             if self.comboBox_4.currentText() == "VBR":
-                preset = str(f'-pix_fmt yuv420p10le -b:v {vbitrate} -maxrate:v {maxrate} -c:v libx265 -preset slow -rc vbr_hq -rc-lookahead 32 -sei hdr -profile:v main10 {colorprim}{transfer}{colormatrix}{color_range}-max_muxing_queue_size 1024 {apreset} -f matroska ')
+                preset = str(f'-pix_fmt yuv420p10le -b:v {vbitrate} -maxrate:v {maxrate} -c:v libx265 -preset slow -rc vbr_hq -rc-lookahead 32 -profile:v main10 {colorprim}{transfer}{colormatrix}{color_range}-max_muxing_queue_size 1024 {apreset} -f matroska ')
             preset_0 = "-hide_banner "
             self.encode_mux_file()
 
